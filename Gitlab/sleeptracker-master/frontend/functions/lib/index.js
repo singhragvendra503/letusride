9dcc2794e4195575ac88be3b2b862c1b3d47a376
singhragvendra503 <124882903+singhragvendra503@users.noreply.github.com>
2023-05-26 16:09:27 +0530

    Delete Gitlab directory

--- Gitlab/sleeptracker-master/frontend/functions/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-//# sourceMappingURL=index.js.map
